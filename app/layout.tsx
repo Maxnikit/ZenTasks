cf999baa0710c473d73f33a4d9ef0a11a5f5ce26
Maxnikit <max.nikit.03@gmail.com>
2024-07-11 19:09:14 +0300

    Add Title

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { MantineProvider, ColorSchemeScript } from '@mantine/core';
 import { theme } from '../theme';
 
 export const metadata = {
-  title: 'Mantine Next.js template',
+  title: 'ZenTasks',
   description: 'I am using Mantine with Next.js!',
 };
 
