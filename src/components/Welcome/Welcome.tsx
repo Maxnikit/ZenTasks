ea550af4c90299f9f543613ab72310dab6ddda98
Maxnikit <max.nikit.03@gmail.com>
2024-07-13 14:41:33 +0300

    Remove Mantine components

--- src/components/Welcome/Welcome.tsx
+++ src/components/Welcome/Welcome.tsx
@@ -1,22 +1,23 @@
-import { Title, Text, Anchor, getPrimaryShade, Button, Center, Stack } from '@mantine/core';
-import Link from 'next/link';
+import Link from "next/link";
 
 export function Welcome() {
   return (
     <div className="flex flex-col gap-5 items-center justify-center mt-20 ">
       <h1 className="text-8xl tracking-tight font-black">
-        Welcome to{' '}
+        Welcome to{" "}
         <span className=" bg-gradient-to-r from-teal-500 to-teal-700 bg-clip-text  text-transparent">
           ZenTasks
         </span>
       </h1>
-      <h2 className="mt-5 text-3xl">Balance your Your Tasks with Ease and Serenity</h2>
-      <Button className="px-6 rounded " component={Link} href="/webapp">
-        Start now
-      </Button>
-      <Button component={Link} href="/login" className="px-6 rounded " mx="auto">
-        TEMPORARY Login
-      </Button>
+      <h2 className="mt-5 text-3xl">
+        Balance your Your Tasks with Ease and Serenity
+      </h2>
+      <Link href="/webapp">
+        <button className="px-6 rounded ">Start now</button>
+      </Link>
+      <Link href="/login">
+        <button className="px-6 rounded ">TEMPORARY Login</button>
+      </Link>
     </div>
   );
 }
